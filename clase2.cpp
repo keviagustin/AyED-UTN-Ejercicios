dd94b29d3752684fd56c3c5cf6cc417632c80814
keviagustin <kevinagustinrockz@gmail.com>
2024-07-24 00:20:19 -0300

    clase2-v1

---
+++ clase2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* Ejercicio 1 */
+    // int x, y;
+
+    // cout << "Ingresa un numero ";
+    // cin >> x;
+    // cout << "Ingresa otro numero ";
+    // cin >> y;
+
+    // cout << x << "+" << y << "= " << x + y << endl;
+    // cout << x << "*" << y << "= " << x * y << endl;
+
+    // if ((x - y) > 0)
+    // {
+    //     cout << x << "-" << y << "= " << x - y << endl;
+    // }
+    // else
+    // {
+    //     cout << "La resta no da mayor que cero" << endl;
+    // }
+
+    /* Ejercicio 2 */
+    // int notas[3], promedio = 0;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     do
+    //     {
+    //         cout << "Ingresar la nota " << i + 1 << " del estudiante entre (0 y 10) ";
+    //         cin >> notas[i];
+    //         if (notas[i] > 10 || notas[i] < 0)
+    //         {
+    //             cout << "Nota invalida. Debe ser entre 0 y 10." << endl;
+    //         }
+    //         else
+    //         {
+    //             promedio += notas[i];
+    //         }
+    //     } while (notas[i] > 10 || notas[i] < 0);
+    // }
+
+    // cout << "El promedio del estudiante es: " << promedio / 3 << endl;
+
+    /* Ejercicio 3 */
+
+    return 0;
+}
---
+++ holaMundo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+namespace primero
+{
+    int x = 1;
+}
+
+int main()
+{
+    // cout << "Hola mundo";
+
+    /* Variables
+    int año = 2024;
+    int dias = 23;
+    int mes = 07;
+
+    double precio = 10.99;
+    double promedio = 2.5;
+
+    float precio2 = 22.98;
+    */
+
+    int x = 0;
+    cout << "X = " << primero::x << endl;
+    cout << "X = " << x;
+
+    return 0;
+}
