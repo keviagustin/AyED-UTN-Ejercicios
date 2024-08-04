52412ef7f26a84961129348aefdd07349f96424d
keviagustin <kevinagustinrockz@gmail.com>
2024-08-03 23:14:42 -0300

    Agregado de ejercicios y parcial completo

--- clase3.cpp
+++ clase3.cpp
@@ -28,6 +28,74 @@ int main()
     // cualEsElMayor(numeros, 10);
 
     /* Ejercicio 2 */
+    // int numeros[50];
+    // int sumaMayores100 = 0;
+    // int conteoMayores100 = 0;
+    // int sumaMenoresMenos10 = 0;
 
+    // for (int i = 0; i < 50; i++)
+    // {
+    //     cout << "Ingresa el numero: " << i + 1 << ": ";
+    //     cin >> numeros[i];
+
+    //     if (numeros[i] > 100)
+    //     {
+    //         sumaMayores100 += numeros[i];
+    //         conteoMayores100++;
+    //     }
+
+    //     if (numeros[i] < -10)
+    //     {
+    //         sumaMenoresMenos10 += numeros[i];
+    //     }
+    // }
+
+    // double promedioMayores100 = 0;
+    // if (conteoMayores100 > 0)
+    // {
+    //     promedioMayores100 = sumaMayores100 / (double)conteoMayores100;
+    // }
+
+    // cout << "El promedio de los números mayores que 100 es: " << promedioMayores100 << endl;
+    // cout << "La suma de los números menores que -10 es: " << sumaMenoresMenos10 << endl;
+
+    /* Ejercicio 3 */
+    int tam = 0;
+    int valor;
+
+    cout << "Ingresa valores positivos (termina con un valor negativo): " << endl;
+
+    int valoresPositivos[100];
+    int valorMaximo = -1;
+    int posicionMaxima = -1;
+
+    while (true)
+    {
+        cin >> valor;
+        if (valor < 0)
+        {
+            break;
+        }
+
+        valoresPositivos[tam] = valor;
+
+        if (valor > valorMaximo)
+        {
+            valorMaximo = valor;
+            posicionMaxima = tam;
+        }
+
+        tam++;
+    }
+
+    if (tam == 0)
+    {
+        cout << "No se ingresaron valores positivos." << endl;
+    }
+    else
+    {
+        cout << "El valor maximo del conjunto es: " << valorMaximo << endl;
+        cout << "La posicion relativa del valor maximo es: " << posicionMaxima + 1 << endl;
+    }
     return 0;
 }
--- parcial1.cpp
+++ parcial1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 using namespace std;
 
 /* Funciones */
@@ -126,6 +127,61 @@ void imprimirTop10(Producto consolidado[], int tam)
     }
 }
 
+/* 4 */
+struct Gol
+{
+    int codigoJugador;
+    int codigoPartido;
+    char nombreApellido[50];
+    char seleccion[30];
+    int cantidadGoles;
+};
+
+struct Jugador
+{
+    int codigoJugador;
+    char nombreApellido[50];
+    int totalGoles;
+};
+
+struct Seleccion
+{
+    char nombre[30];
+    int totalGoles;
+};
+
+int buscarSeleccion(Seleccion selecciones[], int tam, const char nombre[])
+{
+    for (int i = 0; i < tam; i++)
+    {
+        bool iguales = true;
+        for (int j = 0; nombre[j] != '\0' || selecciones[i].nombre[j] != '\0'; j++)
+        {
+            if (nombre[j] != selecciones[i].nombre[j])
+            {
+                iguales = false;
+                break;
+            }
+        }
+        if (iguales)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void copiarCadena(char destino[], const char origen[])
+{
+    int i = 0;
+    while (origen[i] != '\0')
+    {
+        destino[i] = origen[i];
+        i++;
+    }
+    destino[i] = '\0';
+}
+
 int main()
 {
     /* 1 */
@@ -147,43 +203,140 @@ int main()
     // cout << "El total de comisiones generadas fue: " << totalComisionesGeneradas(equipoVentas, ventas, comision) << endl;
 
     /* 3 */
-    Producto deposito1[500], deposito2[500], consolidado[1000];
-    int tam1, tam2, tamConsolidado;
+    // Producto deposito1[500], deposito2[500], consolidado[1000];
+    // int tam1, tam2, tamConsolidado;
 
-    cout << "Ingresa la cantidad de productos en el deposito 1: ";
-    cin >> tam1;
-    cout << "Ingresa la cantidad de productos en el deposito 2: ";
-    cin >> tam2;
+    // cout << "Ingresa la cantidad de productos en el deposito 1: ";
+    // cin >> tam1;
+    // cout << "Ingresa la cantidad de productos en el deposito 2: ";
+    // cin >> tam2;
 
-    for (int i = 0; i < tam1; i++)
+    // for (int i = 0; i < tam1; i++)
+    // {
+    //     cout << "Ingresa el ID del producto " << i + 1 << " en el deposito 1: ";
+    //     cin >> deposito1[i].idProducto;
+    //     cout << "Ingresa la descripcion del producto " << i + 1 << " en el deposito 1: ";
+    //     cin.ignore();
+    //     getline(cin, deposito1[i].descripcion);
+    //     cout << "Ingresa el stock del producto " << i + 1 << " en el deposito 1: ";
+    //     cin >> deposito1[i].stock;
+    // }
+
+    // for (int i = 0; i < tam2; i++)
+    // {
+    //     cout << "Ingresa el ID del producto " << i + 1 << " en el deposito 2: ";
+    //     cin >> deposito2[i].idProducto;
+    //     cout << "Ingresa la descripcion del producto " << i + 1 << " en el deposito 2: ";
+    //     cin.ignore();
+    //     getline(cin, deposito2[i].descripcion);
+    //     cout << "Ingresa el stock del producto " << i + 1 << " en el deposito 2: ";
+    //     cin >> deposito2[i].stock;
+    // }
+
+    // vectorConsolidado(deposito1, tam1, deposito2, tam2, consolidado, tamConsolidado);
+
+    // ordenarPorStock(consolidado, tamConsolidado);
+
+    // ofstream archivo("top10.dat");
+    // archivo << imprimirTop10;
+    // archivo.close();
+
+    /* 4 */
+    ifstream archivo("golesXpartido.dat", ios::binary);
+    if (!archivo)
     {
-        cout << "Ingresa el ID del producto " << i + 1 << " en el deposito 1: ";
-        cin >> deposito1[i].idProducto;
-        cout << "Ingresa la descripcion del producto " << i + 1 << " en el deposito 1: ";
-        cin.ignore();
-        getline(cin, deposito1[i].descripcion);
-        cout << "Ingresa el stock del producto " << i + 1 << " en el deposito 1: ";
-        cin >> deposito1[i].stock;
+        cout << "Error al abrir el archivo." << endl;
+        return 1;
     }
 
-    for (int i = 0; i < tam2; i++)
+    Gol goles[1000];
+    int cantidadGoles = 0;
+
+    while (archivo.read((char *)&goles[cantidadGoles], sizeof(Gol)))
     {
-        cout << "Ingresa el ID del producto " << i + 1 << " en el deposito 2: ";
-        cin >> deposito2[i].idProducto;
-        cout << "Ingresa la descripcion del producto " << i + 1 << " en el deposito 2: ";
-        cin.ignore();
-        getline(cin, deposito2[i].descripcion);
-        cout << "Ingresa el stock del producto " << i + 1 << " en el deposito 2: ";
-        cin >> deposito2[i].stock;
+        cantidadGoles++;
     }
-
-    vectorConsolidado(deposito1, tam1, deposito2, tam2, consolidado, tamConsolidado);
-
-    ordenarPorStock(consolidado, tamConsolidado);
-
-    ofstream archivo("top10.dat");
-    archivo << imprimirTop10;
     archivo.close();
 
+    // a.
+    Jugador maxJugador = {0, "", 0};
+
+    Jugador jugadores[1000];
+    int cantidadJugadores = 0;
+
+    for (int i = 0; i < cantidadGoles; i++)
+    {
+        int j;
+        for (j = 0; j < cantidadJugadores; j++)
+        {
+            if (jugadores[j].codigoJugador == goles[i].codigoJugador)
+            {
+                jugadores[j].totalGoles += goles[i].cantidadGoles;
+                break;
+            }
+            if (j == cantidadJugadores)
+            {
+                jugadores[cantidadJugadores].codigoJugador = goles[i].codigoJugador;
+                copiarCadena(jugadores[cantidadJugadores].nombreApellido, goles[i].nombreApellido);
+                jugadores[cantidadJugadores].totalGoles = goles[i].cantidadGoles;
+                cantidadJugadores++;
+            }
+        }
+        }
+
+    for (int i = 0; i < cantidadJugadores; i++)
+    {
+        if (jugadores[i].totalGoles > maxJugador.totalGoles)
+        {
+            maxJugador = jugadores[i];
+        }
+    }
+
+    cout << "El jugador mas goleador es: " << maxJugador.nombreApellido << " con " << maxJugador.totalGoles << " goles." << endl;
+
+    // b.
+    cout << "Goles por jugador: " << endl;
+    for (int i = 0; i < cantidadJugadores; i++)
+    {
+        cout << jugadores[i].nombreApellido << ": " << jugadores[i].totalGoles << " goles" << endl;
+    }
+
+    // c.
+    Seleccion selecciones[16];
+    int cantidadSelecciones = 0;
+
+    for (int i = 0; i < cantidadGoles; i++)
+    {
+        int idx = buscarSeleccion(Selecciones, cantidadSelecciones, goles[i].seleccion);
+        if (idx == -1)
+        {
+            copiarCadena(selecciones[cantidadSelecciones].nombre, goles[i].seleccion);
+            selecciones[cantidadSelecciones].totalGoles = goles[i].cantidadGoles;
+            cantidadSelecciones++;
+        }
+        else
+        {
+            selecciones[idx].totalGoles += goles[i].cantidadGoles;
+        }
+    }
+
+    // Encontrar las selecciones con menos goles
+    int minGoles = selecciones[0].totalGoles;
+    for (int i = 1; i < cantidadSelecciones; i++)
+    {
+        if (selecciones[i].totalGoles < minGoles)
+        {
+            minGoles = selecciones[i].totalGoles;
+        }
+    }
+
+    cout << "Las selecciones que convirtieron menos goles son: " << endl;
+    for (int i = 0; i < cantidadSelecciones; i++)
+    {
+        if (selecciones[i].totalGoles == minGoles)
+        {
+            cout << selecciones[i].nombre << ": " << selecciones[i].totalGoles << " goles" << endl;
+        }
+    }
     return 0;
 }
